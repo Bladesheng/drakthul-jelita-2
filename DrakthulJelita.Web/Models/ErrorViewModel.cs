f2790ac92c68cb05fac311b32159a5fa28253bae
Bladesheng <keadr23@gmail.com>
2026-02-28 09:14:18 +0100

    init

---
+++ DrakthulJelita.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DrakthulJelita.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
